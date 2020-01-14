69461d904ea772e0eec1d3c480f083ce19986fa7
Himanshu <Hershey@Harshitas-MacBook-Air.local>
2020-01-14 12:59:09 -0500

    Changed the version

--- connect.php
+++ connect.php
@@ -27,7 +27,7 @@
 <body>
   <div align="center">
     <h1>Hello DHHS Cloud Team</h1>
-    <h2>This is version 3.0 of Demo Application</h2>
+    <h2>This is version 3.1 of Demo Application</h2>
     </div>
   </body>
 </html>
